dbb1c573ad3ea5e0f3f6886addd68bf664416485
bekbeis <bekzat.beis@gmail.com>
2024-03-16 17:37:20 +0500

    feat: added function to create verilog buffer

--- scripts/main.py
+++ scripts/main.py
@@ -8,47 +8,8 @@ from utils import *
 
 
 def main():
-    # file_path = sys.argv[1]
-
-    # assign_statements, always_blocks = parse_verilog_file(file_path)
-
-    # i = 1
-
-    # test_assign = list(assign_statements.items())[i]
-    # original_code, updated_code, line_number, bug_description = request_bug(
-    #     test_assign[1])
-
-    # reconstruct_verilog_file(file_path, test_assign[0] - 1,
-    #                     original_code, updated_code, line_number - 1)
-
-    # print()
-    # print(
-    #     f"[ Check from line {list(assign_statements.keys())[i]} and further ]\n")
-
-    # test_always = list(always_blocks.items())[i]
-    # original_code, updated_code, line_number, bug_description = request_bug(
-    #     test_always[1])
-
-    # reconstruct_verilog_file(file_path, test_always[0] - 1,
-    #                     original_code, updated_code, line_number - 1)
-
-    # print()
-    # print(f"[ Check from line {list(always_blocks.keys())[i]} and further ]\n")
-
-    # print("ORIGINAL CODE:")
-    # print(original_code)
-    # print()
-    # print("UPDATED CODE:")
-    # print(updated_code)
-    # print()
-    # print("BUG DESCRIPTION:")
-    # print(bug_description)
-
-    # replace_file(file_path)
-
-    directory_path = "/home/maveric/workspace/cva6/core"
-    verilog_buffer = parse_dir(directory_path)
-    export_to_json(verilog_buffer)
+    directory_path = "/home/maveric/workspace/copies/cva6/"
+    create_verilog_buffer(directory_path)
 
 
 main()
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,25 +4,24 @@
 import os
 import json
 import subprocess
+import multiprocessing
 from verilog_operations import parse_verilog_file
 
 
 def replace_file(file_path):
-    fp_splitted = file_path.split('/')
-    cmd1 = ['mv', file_path, '../original-files/']
-    cmd2 = ['mv', fp_splitted[-1], '/'.join(fp_splitted[:-1])]
-    subprocess.run(cmd1)
-    subprocess.run(cmd2)
+    directory, filename = os.path.split(file_path)
+    subprocess.run(['mv', file_path, '../original-files/'])
+    subprocess.run(['mv', filename, directory])
 
 
 def parse_dir(directory):
-    results = dict()
+    results = {}
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".v") or file.endswith(".sv"):
                 file_path = os.path.join(root, file)
-                # print(f"Processing file: {file_path}")
-                assign_statements, always_blocks = parse_verilog_file(file_path)
+                assign_statements, always_blocks = parse_verilog_file(
+                    file_path)
                 results[file_path] = {
                     "assign_statements": assign_statements,
                     "always_blocks": always_blocks
@@ -30,6 +29,7 @@ def parse_dir(directory):
     return results
 
 
-def export_to_json(dictionary):
+def create_verilog_buffer(directory):
+    verilog_buffer = parse_dir(directory)
     with open("buffer.json", "w") as outfile:
-        json.dump(dictionary, outfile)
+        json.dump(verilog_buffer, outfile)
